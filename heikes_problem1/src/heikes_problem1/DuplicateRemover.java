8cda3a3f559706cd061ad6eba0e6fe03ed3a4f59
nheikes <noah.heikes@knights.ucf.edu>
2020-03-29 17:54:53 -0400

    init commit

---
+++ heikes_problem1/src/heikes_problem1/DuplicateRemover.java
@@ -0,0 +1,62 @@
+package heikes_problem1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.*;
+
+public class DuplicateRemover {
+	
+	private Set<String> uniqueWords = new HashSet<String>();
+	
+	public void remove(String dataFile)
+	{
+		Set<String> duplicates = new HashSet<String>();
+		
+		try 
+		{
+			Scanner in = new Scanner(new File(dataFile));
+			
+			String holdWord;
+			while (in.hasNext())
+			{
+				holdWord = in.next();
+				if (!uniqueWords.contains(holdWord))
+				{
+					uniqueWords.add(holdWord);
+				}
+			}
+			
+		} 
+		catch (FileNotFoundException e) 
+		{
+			System.out.println("FileNotFoundException - That file does not exist.");
+			return;
+		}
+	}
+	
+	public void write(String dataFile)
+	{
+		try {
+			String output = "";
+			for (String string : uniqueWords) 
+			{
+				output += string + " ";
+			}
+			output.trim();
+		    try (FileOutputStream outputStream = new FileOutputStream(dataFile))
+		    {
+		    	
+		    	outputStream.write(output.getBytes());
+		    }
+		    
+		}
+		catch (IOException e)
+		{
+			System.out.println("IOException - the program will now stop.");
+			return;
+		}
+	}
+
+}
---
+++ heikes_problem2/src/heikes_problem2/DuplicateCounter.java
@@ -0,0 +1,66 @@
+package heikes_problem2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class DuplicateCounter 
+{
+	HashMap<String, Integer> wordCounter = new HashMap<String, Integer>();
+	
+	public void count(String dataFile)
+	{
+		Set<String> duplicates = new HashSet<String>();
+		
+		try 
+		{
+			Scanner in = new Scanner(new File(dataFile));
+			
+			String holdWord;
+			Integer counter;
+			while (in.hasNext())
+			{
+				holdWord = in.next();
+				counter = wordCounter.putIfAbsent(holdWord, 1);
+				if (counter != null)
+				{
+					wordCounter.replace(holdWord, ++counter);
+				}
+			}
+			
+		} 
+		catch (FileNotFoundException e) 
+		{
+			System.out.println("FileNotFoundException - That file does not exist.");
+			return;
+		}
+	}
+	
+	public void write(String dataFile)
+	{
+		try {
+			String output = "";
+			for (String string : wordCounter.keySet()) 
+			{
+				output += "Key: \"" + string + "\", Count: " + wordCounter.get(string) + " \n";
+			}
+			output.trim();
+		    try (FileOutputStream outputStream = new FileOutputStream(dataFile))
+		    {
+		    	
+		    	outputStream.write(output.getBytes());
+		    }
+		    
+		}
+		catch (IOException e)
+		{
+			System.out.println("IOException - the program will now stop.");
+			return;
+		}
+	}
+}
